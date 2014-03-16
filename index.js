e56a8c7fa7b98fca3f114690764c36db86e56216
Półtorak Dariusz <poltorak.dariusz@gmail.com>
2014-03-16 11:46:09 +0100

    verb register

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var verbRegister = require(__dirname + '/src/verbRegister.js'),
+    routeHandler = require(__dirname + '/src/routeHandler.js'),
+    pathHandler = require(__dirname + '/src/pathHandler.js');
+
+module.exports = function () {
+
+    this.createRouter = function () {
+
+    };
+
+};
---
+++ src/verbRegister.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var routeHandler = require(__dirname + '/route.js');
+
+function VerbRegister() {
+
+    /**
+     * HTTP methods with routes assigned to them
+     * @type {{}}
+     */
+    var verbs = {};
+
+    /**
+     * HTTP methods with routes assigned to them by name
+     * @type {{}}
+     */
+    var verbsByName = {};
+
+    var allRoutes = [];
+
+    /**
+     * Create setter for VERB methods
+     * @param name
+     * @returns {Function}
+     */
+    function createVerbSetter(verb) {
+        return function (name, path, requirements, data) {
+            var route = routeHandler.create(name, path, requirements, data);
+
+            if (!verbs[verb]) {
+                verbs[verb] = [];
+            }
+            if (!verbsByName[verb]) {
+                verbsByName[verb] = {};
+            }
+            if (verbsByName[verb][route.name]) {
+                throw new Error('Route "' + String(route.name) + '" already registered!');
+            }
+
+            verbs[verb].push(route);
+            verbsByName[verb][route.name] = route;
+            allRoutes.push(route);
+
+            return this;
+        };
+    }
+
+    this.get = createVerbSetter('get');
+    this.post = createVerbSetter('post');
+    this.put = createVerbSetter('put');
+    this['delete'] = createVerbSetter('delete');
+    this.options = createVerbSetter('options');
+    this.head = createVerbSetter('head');
+    this.trace = createVerbSetter('trace');
+    this.connect = createVerbSetter('connect');
+    this.any = createVerbSetter('any');
+
+    this.findByName = function (name, verbType) {
+        var route;
+        if (verbType) {
+            route = verbsByName[verbType] && verbsByName[verbType][name] ? verbsByName[verbType][name] : false;
+        } else {
+            var i;
+            route = false;
+            for (i = 0; i < allRoutes.length; i++) {
+                if (allRoutes[i] === name) {
+                    route = allRoutes[i];
+                    break;
+                }
+            }
+        }
+        return route;
+    };
+
+    this.math = function (path, verbType) {
+        var route = false,
+            i;
+        if (verbType) {
+            for (i = 0; i < verbs[verbType].length; i++) {
+                if (routeHandler.match(path, verbs[verbType][i])) {
+                    route = verbs[verbType][i];
+                    break;
+                }
+            }
+        } else {
+            for (i = 0; i < allRoutes.length; i++) {
+                if (routeHandler.match(path, allRoutes[i])) {
+                    route = allRoutes[i];
+                    break;
+                }
+            }
+        }
+        return route;
+    };
+}
+
+module.exports = {
+    create: function (options) {
+        return new VerbRegister(options || {});
+    },
+    VerbRegister: VerbRegister
+};
